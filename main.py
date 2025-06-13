ffd0ffdfec21f148727fe453e60ebfa0799d95b9
R.D.K.S. Kumara <134190302+sanjayakasun@users.noreply.github.com>
2025-06-13 14:46:20 +0530

    new up

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+import uvicorn
+import io
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
+model = load_model("model/best_fish_classifier.h5")
 
+class_names = ["Bulath_hapaya", "Dankuda_pethiya", "Depulliya", "Halamal_dandiya", "Lethiththaya", "Pathirana_salaya", "Thal_kossa"]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def preprocess_image(image_bytes):
+    img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    img = img.resize((224, 224))  # depends on your model input
+    img_array = np.array(img) / 255.0
+    return np.expand_dims(img_array, axis=0)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    img_tensor = preprocess_image(image_bytes)
+    prediction = model.predict(img_tensor)[0]
+    predicted_index = np.argmax(prediction)
+    predicted_class = class_names[predicted_index]
+    return JSONResponse(content={"prediction": predicted_class})
